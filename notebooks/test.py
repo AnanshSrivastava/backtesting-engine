7b0392b2997c676621e40b0ab187c01a1f810b29
AnanshSrivastava <yesiam.anansh@gmail.com>
2026-08-16 02:07:59 +0530

    added strategy of SMA crossover detection  and verbose flag

--- notebooks/test.py
+++ notebooks/test.py
@@ -4,9 +4,16 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.fetch_data import fetch_data
+from src.strategy import generate_signals
 
-df = fetch_data("RELIANCE.NS", "2023-01-01", "2024-01-01")
+df = fetch_data("RELIANCE.NS", "2020-01-01", "2024-01-01")
 if df is not None:
-    print(df.head())
+    print("--- verbose=True ---")
+    df_signals = generate_signals(df.copy(), verbose=True)
+
+    print("--- verbose=False ---")
+    df_signals = generate_signals(df.copy(), verbose=False)
+    df_signals = df_signals.dropna(subset=['SMA_long'])  # Drop rows where SMA values are NaN
+    print(df_signals.tail())
 else:
     print("No data for RELIANCE.NS")
